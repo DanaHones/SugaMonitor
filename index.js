456c7e5527b14274da689876ce885c2408346dde
danahones <danahones@gmail.com>
2018-03-30 14:59:06 -0400

    Runs

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ module.exports = {
 var clientId = 'p4VzZW3nTxeklmtOidZO6w'
 var clientSecret = 'G1in6mg_RO2tjaCOby7sLg'
 
-
-
-
-//console.log("Starting...");
-//auth.simple(clientId, clientSecret).then(function (client) {
-//  console.log(auth);
-//  client.defaultSensorId().then(function (sensorId) {
-//    client.liveSamples(sensorId).then(function (samples) {
-//      console.log(samples)
-//    })
-//  })
-//});
+console.log("Starting...");
+auth.simple(clientId, clientSecret).then(function (client) {
+  console.log(auth);
+  client.defaultSensorId().then(function (sensorId) {
+    client.liveSamples(sensorId).then(function (samples) {
+      console.log(samples)
+    })
+  })
+});
